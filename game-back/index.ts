19892764d1dbfbf10b7c837da34565bd1a14c07f
marius-ar <mariusaillerie@gmail.com>
2023-11-14 09:15:39 +0100

    Working Socket

--- game-back/index.ts
+++ game-back/index.ts
@@ -1,24 +1,13 @@
-import {WebSocketServer} from 'ws';
+import {Server} from 'socket.io';
+import {createServer} from 'http';
 
-const wss = new WebSocketServer({
-    port: 8080,
-    perMessageDeflate: {
-        zlibDeflateOptions: {
-            // See zlib defaults.
-            chunkSize: 1024,
-            memLevel: 7,
-            level: 3
-        },
-        zlibInflateOptions: {
-            chunkSize: 10 * 1024
-        },
-        // Other options settable:
-        clientNoContextTakeover: true, // Defaults to negotiated value.
-        serverNoContextTakeover: true, // Defaults to negotiated value.
-        serverMaxWindowBits: 10, // Defaults to negotiated value.
-        // Below options specified as default values.
-        concurrencyLimit: 10, // Limits zlib concurrency for perf.
-        threshold: 1024 // Size (in bytes) below which messages
-        // should not be compressed if context takeover is disabled.
-    }
+const httpServer = createServer();
+const io = new Server(httpServer);
+
+io.on('connection', socket => {
+    console.log('connected');
+});
+
+httpServer.listen(3000, () => {
+    console.log(`listening on port ${3000}`);
 });
